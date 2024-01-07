870b09c3683c7ed1feb3f39f4d100aa2ccc664ef
T.M <ace071144@gmail.com>
2024-01-07 09:57:48 +0900

    jquery導入

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-import "net/http"
+import(
+  "github.com/gin-gonic/gin"
+  "net/http"
+)
 
 func main() {
-    engine:= gin.Default()
-    engine.LoadHTMLGlob("templates/*")
-    engine.GET("/", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.html", gin.H{
-        })
-    })
-    engine.Run(":3000")
+  engine := gin.Default()
+  engine.Static("/css", "./static/css")
+  engine.Static("/js", "./static/js")
+  engine.LoadHTMLGlob("templates/*.html")
+  engine.GET("/", func(ctx *gin.Context) {
+    ctx.HTML(http.StatusOK, "index.html", gin.H{})
+  })
+  engine.Run(":3000")
 }
